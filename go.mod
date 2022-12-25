4e48fb8ada532c14eeeccfe57df6b6310b3031f6
sp3ct0r.one <git.pk@sp3ct0r.one>
2022-12-26 04:17:48 +0700

    add crud operation on v2

--- go.mod
+++ go.mod
@@ -11,10 +11,13 @@ require (
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
@@ -22,4 +25,6 @@ require (
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/sys v0.2.0 // indirect
+	gorm.io/driver/sqlite v1.4.3 // indirect
+	gorm.io/gorm v1.24.2 // indirect
 )
--- router/router.go
+++ router/router.go
@@ -4,7 +4,10 @@ import (
 	m "A-Simple-Api-Go-Fiber/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"gorm.io/gorm/clause"
 )
+
+var db, _ = m.DbInit()
 
 // CreateRoutes for fiber app
 func CreateRoutes(app *fiber.App) {
@@ -16,12 +19,19 @@ func CreateRoutes(app *fiber.App) {
 	v1.Get("/info", info)
 	v1.Get("/connections", connections)
 
-	// TODO Api v2 Restful
-
+	// Api v2 Restful
+	v2 := api.Group("/v2")
+	v2.Get("/cars", getCars)
+	v2.Post("/cars", createCar)
+	v2.Put("/cars", updateCar)
+	v2.Delete("/cars", deleteCar)
+	v2.Get("/cars/:id<int>", getCarWithPath)
+	v2.Put("/cars/:id<int>", updateCarWithPath)
+	v2.Delete("/cars/:id<int>", deleteCarWithPath)
 	// TODO Api v3 Auth Jwt
 }
 
-// Ping endpoint Api v1
+// Endpoint Api v1
 func ping(c *fiber.Ctx) error {
 	return c.JSON(m.GetPong(c))
 }
@@ -32,4 +42,80 @@ func info(c *fiber.Ctx) error {
 
 func connections(c *fiber.Ctx) error {
 	return c.JSON(m.GetConnections())
+}
+
+// GetCars Endpoint Api v2
+func getCars(c *fiber.Ctx) error {
+	id := c.Query("id")
+	if id == "" {
+		var cars []m.Car
+		db.Find(&cars)
+		return c.JSON(m.AllCars{Total: len(cars), Cars: cars})
+	} else {
+		var car m.Car
+		db.First(&car, id)
+		return c.JSON(car)
+	}
+}
+
+func createCar(c *fiber.Ctx) error {
+	var car m.Car
+	var records int64
+	if err := c.BodyParser(&car); err != nil {
+		return err
+	}
+	db.Model(&[]m.Car{}).Count(&records)
+	if records >= 20 {
+		m.DbReset(db)
+	}
+	db.Create(&car)
+	return c.JSON(car)
+}
+
+func updateCar(c *fiber.Ctx) error {
+	id := c.Query("id")
+	var car m.Car
+	if err := c.BodyParser(&car); err != nil {
+		return err
+	}
+	if id == "" {
+		db.Model(&car).Updates(car)
+		return c.JSON(car)
+	} else {
+		db.Model(&car).Clauses(clause.Returning{}).Where("id = ?", id).Updates(car)
+		return c.JSON(car)
+	}
+}
+
+func deleteCar(c *fiber.Ctx) error {
+	id := c.Query("id")
+	var car m.Car
+	db.First(&car, id)
+	db.Delete(&car)
+	return c.JSON(car)
+}
+
+func getCarWithPath(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var car m.Car
+	db.First(&car, id)
+	return c.JSON(car)
+}
+
+func updateCarWithPath(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var car m.Car
+	if err := c.BodyParser(&car); err != nil {
+		return err
+	}
+	db.Model(&car).Clauses(clause.Returning{}).Where("id = ?", id).Updates(car)
+	return c.JSON(car)
+}
+
+func deleteCarWithPath(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var car m.Car
+	db.First(&car, id)
+	db.Delete(&car)
+	return c.JSON(car)
 }
